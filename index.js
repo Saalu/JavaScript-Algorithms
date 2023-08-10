f1790aaf5ed1e393f0c0ecb9e8a257ad288de395
Saalu <saaluissaka@gmail.com>
2023-08-10 21:40:15 +0000

    factorial of a number

--- index.js
+++ index.js
@@ -6,9 +6,22 @@ function fibonacci(n) {
   return fib;
 }
 
-// console.log(fibonacci(2));
-console.log(fibonacci(3));
-console.log(fibonacci(5));
-console.log(fibonacci(7));
-console.log(fibonacci(8));
-console.log(fibonacci(9));
+// // console.log(fibonacci(2));
+// console.log(fibonacci(3));
+// console.log(fibonacci(5));
+// console.log(fibonacci(7));
+// console.log(fibonacci(8));
+// console.log(fibonacci(9));
+
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(3));
+console.log(factorial(5));
